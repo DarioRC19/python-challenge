2dacbfa142d557263aab0052fef4b423f6abcfa4
DarioRC19 <darioFRC19@gmail.com>
2023-04-10 16:52:17 -0400

    comments added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,22 @@
+#Modules
 import os
 
 
 import csv
 
-
+#Path for file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+#Open CSV
 with open(csvpath) as csvfile:
 
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    
+    #Skips header
     next(csvreader)
 
+    #Setting up variables we are looking for
     total_months = 1
 
     total_profit = 0
@@ -24,17 +27,22 @@ with open(csvpath) as csvfile:
 
     greatest_decrease = 9999999999999999999
 
+    #We set these print statements to make it look as close to the example that was given
     print()
     print("Finacial Analysis")
     print()
     print('-' * 20)
     print()
+    
+    #The CSV reader will start at (2,2) instead of (2,1), helps finding average total change
     row = next(csvreader)
 
     previous_value = float(row[1])
 
+    #For loop established in CSV file
     for row in csvreader:
-
+        
+        #Setting up Counters
         total_months += 1
         total_profit += int(row[1])
         current_value = float(row[1])
@@ -45,7 +53,7 @@ with open(csvpath) as csvfile:
 
         total_changes += change
 
-       
+       #if statement that will give us the month that max increase and decrease happend along with the month it happened in 
         if change > greatest_increase:
             greatest_increase = int(change)
             greatest_increase_month = row[0]
@@ -54,10 +62,11 @@ with open(csvpath) as csvfile:
         if change < greatest_decrease:
             greatest_decrease = int(change)
             greatest_decrease_month = row[0]
-
+    
+    #One is minused from the total months because changes does not occur in the 1st month
     average_profit = (total_changes)  / (total_months - 1)
 
-
+    #Print function used to print results in cmd
     print(f'Total Months: {total_months}')
     print()
     print(f'Total: ${total_profit}')
@@ -69,8 +78,10 @@ with open(csvpath) as csvfile:
     print(f'Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})')
     print()
 
+#Opened a txt file to the analysis folder that will be writen on
 with open('analysis/budget_data.txt','w') as txtfile:
 
+    #Used the txtfile.write function to write the results in the file and \n to organize and make file presentable and legible 
     txtfile.write('\n')
     txtfile.write("Finacial Analysis\n")
     txtfile.write('\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,59 +1,73 @@
-
+#Modules
 import os
 
 
 import csv
 
-
+#Path for file
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-
+#Open CSV
 with open(csvpath) as csvfile:
 
    
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    
+    #Skips header
     next(csvreader)
 
+    #Blank dictionary established where info from file will be used/stored
     election = {}
 
+    #For loop that combined all in info in row2
     for row in csvreader:
 
         name = row[2]
 
+        #IF statement that made a counter for each name to get a total for each & overall total in the election dictionary
         if name in election:
             election[name] += 1
         else:
             election[name] = 1 
 
+#Total variable establiehed 
 total = 0
 
+#We set these print statements to make it look as close to the example that was given
 print()
 print("Election Results")
 print()
 print( '-' * 20 )
 
+#Max used to find name with most votes
 winner = max(election, key=election.get)
 
+#Used For loop on the election dictionary to get total sum the votes
 for name,count in election.items():
     total += count
 
+#print function to print results
 print(f'Total Votes: {total}')   
 print() 
 print( '-' * 20 )
+
+#Used for loop in election dictonary to find how much each canidate got used info to get percentage
 for name,count in election.items():
     percent = (count / total) *100
-    
+
+    #Used Print Function to print the variables below into the cmd
     print(f"{name}: {percent: .3f}% ({count})")
     print()
 
 print('-' * 20)
+#Print function used to print winner
 print(f'Winner: {winner}')
 
 
-
+#Opened a txt file to the analysis folder that will be writen on
 with open('analysis/election_data.txt','w') as txtfile:
     
+    #Used the txtfile.write the results in the file and \n to organize and make file presentable and legible
     txtfile.write('\n')
     txtfile.write('Election Results\n')
     txtfile.write('\n')
